52cd7133f1972c01cc351e01c5e6b6e0dfc2085c
Nibin <nibinjohn29@gmail.com>
2025-10-30 16:43:54 +0530

    Square of number

---
+++ squareofnum.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Ask how many numbers
+read -p "Enter number of elements: " n
+
+# Create an empty array (stack)
+stack=()
+
+# Take inputs from user
+for (( i=1; i<=n; i++ )); do
+    read -p "Enter number $i: " val
+    stack+=($val)
+done
+
+# Calculate and display squares
+for num in "${stack[@]}"; do
+    square=$((num * num))
+    echo "The square of $num is $square"
+done
+
