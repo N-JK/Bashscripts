3bf4285b4dd0b77797fe6d74ef18ef68512b6c22
Nibin <nibinjohn29@gmail.com>
2025-11-06 06:16:13 +0530

    second_largest_6:16

--- second_highest.sh
+++ second_highest.sh
@@ -3,3 +3,8 @@
 
 #read total count of numbers
 read -p "Enter the total count of numbers: " count
+
+#validate input
+if [ $count -lt 2 ]; then
+    echo "you need to enter at least 2 numbers."
+    
