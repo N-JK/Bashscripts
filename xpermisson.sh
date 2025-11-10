cddf6fc1874083c3d070b47eb556fe15f7a01209
Nibin <nibinjohn29@gmail.com>
2025-11-10 07:38:08 +0530

    permission_7:38

--- xpermisson.sh
+++ xpermisson.sh
@@ -5,4 +5,8 @@
 for file in *; do
     if [ -f "$file" ]; then
         if [ ! -x  "$file" ]; then
-	    	
+	    echo "Adding execute permission to: $file"
+       	    chmod +x "$file"
+	else
+	    echo "$file already has execute permission."
+	fi
